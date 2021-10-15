e1820281812a927584a94fc1ffa061e6309138f1
Mitya <you@example.com>
2021-10-15 14:08:59 +0300

    Tasks from module one

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,41 @@
 package com.company;
-import java.util.Scanner;
-public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.println(a+b);
+import java.util.Arrays;
+import java.util.Scanner;
+class Main {
+    public static int[] minToBegin(int[] inputArray){
+        int min = inputArray[0];
+        int index = 0;
+        int[] res = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++)
+        {
+            if (inputArray[i] < min) {
+                min = inputArray[i];
+                index = i;
+            }
+        }
+        res[0] = min;
+        boolean flag = true;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i == index) {
+                flag = false;
+                continue;
+            }
+            if (flag)
+                res[i + 1] = inputArray[i];
+            else
+                res[i] = inputArray[i];
+        }
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = res[i];
+        }
+        return res;
     }
+    public static void main(String[] args){
+        int[] array = {200, -5555, 4852, -5555, 15, 78, 65};
+        System.out.println(Arrays.toString(minToBegin(array)));
+        System.out.println(Arrays.toString(array));
+
+    }
+
 }
