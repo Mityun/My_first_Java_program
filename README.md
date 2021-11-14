b31c34e07de53aa67c2a0918062d1611c95dcb26
Mitya <you@example.com>
2021-11-14 18:11:17 +0300

    Sending xmlfile to Hometask_XMLeducationSystem

--- README.md
+++
@@ -1,2 +0,0 @@
-# Samsung-IT-school
-F*cking Java... I ll do my best to earn gold certificate))0)
--- src/bank/erros/BankServiceTypeException.java
+++
@@ -1,7 +0,0 @@
-package bank.erros;
-
-public class BankServiceTypeException extends Exception {
-    public BankServiceTypeException(String errorMessage) {
-        super(errorMessage);
-    }
-}
